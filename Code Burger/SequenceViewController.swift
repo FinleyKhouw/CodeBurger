27d262ad3d154e77a6c1689bee8c5515a4790387
Mirza Fachreza <mirzaf99@gmail.com>
2019-07-17 16:46:50 +0700

    Add Mirza's Source Codes

---
+++ Code Burger/SequenceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  Sorting Game CodeBurger
+//
+//  Created by Mirza Fachreza 2 on 09/07/19.
+//  Copyright © 2019 Mirza Fachreza. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class SequenceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'PrologueScene.sks'
+            if let scene = SKScene(fileNamed: "SequenceStage") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}

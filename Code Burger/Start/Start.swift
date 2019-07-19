73c95acbc079b4e1e39a5aacb722da25d67ac80d
Slamet Riyadi <slametngeblog@gmail.com>
2019-07-19 15:13:38 +0700

    Update Start source code

--- Code Burger/Start/Start.swift
+++ Code Burger/Start/Start.swift
@@ -13,7 +13,10 @@ class Start: SKScene {
     let burger = SKSpriteNode(imageNamed: "burger")
     let playButton = SKSpriteNode(imageNamed: "play")
     let stageButton = SKSpriteNode(imageNamed: "stage-select")
+    
+    let pausePage = SKSpriteNode(imageNamed: "pausepage")
     let userDef = UserDefaults.standard
+    var reuseIdentifier:[String] = ["sequenceStage", "commandStage", "mazeStage"]
     
     override func didMove(to view: SKView) {
         self.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
@@ -49,18 +52,17 @@ class Start: SKScene {
             if touchedNode == playButton {
                     if let view = self.view {
                         if userDef.bool(forKey: "hasCharacter") {
-                            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-                            let sequenceController = storyBoard.instantiateViewController(withIdentifier: "sequenceStage")
-                            self.view?.window?.rootViewController!.present(sequenceController, animated: true, completion: nil)
-                            
-//                            let scene = StageSelect(size: view.frame.size)
-//                            
-//                            scene.scaleMode = .aspectFill
-//                            view.presentScene(scene,transition: SKTransition.fade(withDuration: 0.5))
-//                            view.ignoresSiblingOrder = true
-//                            
-//                            view.showsFPS = false
-//                            view.showsNodeCount = true
+                            switch userDef.integer(forKey: "stageIndex") {
+                            case 0:
+                                print(reuseIdentifier[0])
+                                changeScene(resueIdentifier: reuseIdentifier[0])
+                            case 1:
+                                changeScene(resueIdentifier: reuseIdentifier[1])
+                            case 2:
+                                changeScene(resueIdentifier: reuseIdentifier[2])
+                            default:
+                                print("no index detected")
+                            }
                         } else {
                             let scene = CharacterSelect(size: view.frame.size)
                             
@@ -87,7 +89,53 @@ class Start: SKScene {
                         
                     }
                 }
+            } else if touchedNode == burger {
+                if let view = self.view {
+                    // Create an effects node with a gaussian blur filter
+                    let effectsNode = SKEffectNode()
+                    let filter = CIFilter(name: "CIGaussianBlur")
+                    // Set the blur amount. Adjust this to achieve the desired effect
+                    let blurAmount = 10.0
+                    filter?.setValue(blurAmount, forKey: kCIInputRadiusKey)
+                    
+                    effectsNode.filter = filter
+                    effectsNode.position = self.view!.center
+                    effectsNode.blendMode = .alpha
+                    
+                    // Create a sprite
+                    let texture = SKTexture(imageNamed: "Spaceship")
+                    let sprite = SKSpriteNode(texture: texture)
+                    
+                    // Add the sprite to the effects node. Nodes added to the effects node
+                    // will be blurred
+                    effectsNode.addChild(sprite)
+                    // Add the effects node to the scene
+                    self.addChild(effectsNode)
+                    
+                    // Create another sprite
+                    let sprite2 = SKSpriteNode(texture: texture)
+                    sprite2.position = self.view!.center
+                    sprite2.size = CGSize(width:64, height:64);
+                    sprite2.zPosition = 100
+                    
+                    // Add the sprite to the scene. Nodes added to the scene won't be blurred
+                    self.addChild(sprite2)
+                    
+                    
+                    pausePage.zPosition = 2
+                    pausePage.size = CGSize(width: 400, height: 400)
+                    pausePage.position = CGPoint(x: size.width/2, y: size.height/2)
+                    addChild(pausePage)
+                
+                }
             }
         }
+    }
+    
+    func changeScene(resueIdentifier: String) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let sequenceController = storyBoard.instantiateViewController(withIdentifier:
+        resueIdentifier)
+        self.view?.window?.rootViewController!.present(sequenceController, animated: true, completion: nil)
     }
 }
